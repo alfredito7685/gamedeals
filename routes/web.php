f0d4672e6610ef562cb61a938408b8610934397d
Alfredo <alfredo82552@gmail.com>
2026-07-10 10:46:51 -0600

    Arigato

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Google\Auth\Credentials\ServiceAccountCredentials;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/gameDeals', function () {
+    return "Bienvenido a GameDeals, la mejor página para encontrar ofertas de videojuegos.";
+});
+
+Route::get('/probar-firebase', function () {
+    $rutaCredenciales = storage_path('app/firebase_credentials.json');
+
+    $credenciales = new ServiceAccountCredentials(
+        'https://www.googleapis.com/auth/datastore',
+        $rutaCredenciales
+    );
+
+    $token = $credenciales->fetchAuthToken();
+    $accessToken = $token['access_token'];
+
+    $projectId = 'gamedeals-84757';
+
+    $response = Http::withToken($accessToken)->post(
+        "https://firestore.googleapis.com/v1/projects/{$projectId}/databases/(default)/documents/pruebas",
+        [
+            'fields' => [
+                'mensaje' => ['stringValue' => 'Conexión exitosa con GameDeals'],
+                'fecha' => ['stringValue' => now()->toDateTimeString()],
+            ]
+        ]
+    );
+
+    return $response->json();
 });
